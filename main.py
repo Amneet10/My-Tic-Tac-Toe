c2b59f7c05b2cdb2b04f9839637a58c4974fc78c
Amneet Kaur <kauramneet986@gmail.com>
2022-09-08 10:37:16 -0400

    Tic-tak-toe

---
+++ main.py
@@ -0,0 +1,16 @@
+
+v1=''
+v2='X'
+v3='0'
+v4='X'
+v5='0'
+v6=''
+v7='X'
+v8='0'
+v9=''
+originalGame = f'{v1} | {v2} | {v3} | \n {v4} | {v5} | {v6} |\n{v7} | {v8} | {v9} |'
+print(originalGame)
+
+
+
+
