d2d6aed6cdfd1d5e57608dfd3e9afde5e878ca86
Amneet Kaur <kauramneet986@gmail.com>
2022-09-12 13:30:48 -0400

    Tic-tak-toe

--- main1.py
+++ main1.py
@@ -14,7 +14,7 @@
 # userInput = int(input("PlayerOne: Enter your Turn" ))
 
 def printFunction(grid):
-    print(f'{grid[1]}|{grid[2]}|{grid[3]}|\n -----{grid[4]}|{grid[5]}|{grid[6]}\n -----{grid[7]}|{grid[8]}|{grid[9]}')
+    print(f'{grid[1]} | {grid[2]} | {grid[3]}\n -----\n{grid[4]} | {grid[5]} | {grid[6]}\n -----\n{grid[7]} | {grid[8]} | {grid[9]}')
    
 
 
@@ -28,17 +28,27 @@ flag = True
 while True:
     if flag:
         userInput1 = int(input("first player turn : select your box\n"))
-        listOfBoxes[userInput1] = 'X'
-        printFunction(listOfBoxes)
-        flag=False
+        if listOfBoxes[userInput1] in ('X','o'):
+            print("value is already entered.Plaese choose a differnt number")
+        else:
+            listOfBoxes[userInput1] = 'X'
+            printFunction(listOfBoxes)
+            flag=False
         
 
     else:
         userInput2 = int(input("second player turn : select your box\n"))
-        listOfBoxes[userInput2] = 'o'
-        printFunction(listOfBoxes)
-        flag=True
-    print(listOfBoxes)
+        if listOfBoxes[userInput2] in ('X','o'):
+            print("value is already entered.Plaese choose a differnt number")
+        else:
+            listOfBoxes[userInput2] = 'o'
+            printFunction(listOfBoxes)
+            flag=True
+
+
+ 
+
+
 
 
 
